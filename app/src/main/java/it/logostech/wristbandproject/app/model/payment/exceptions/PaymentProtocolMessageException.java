e53091fbb252111f871d2e984e0e21ab52f2e4ce
Michele Schimd <skimmoz@gmail.com>
2014-09-04 12:59:19 +0200

    Completed protocol for wear, still actual communication methods need to be implemented

---
+++ app/src/main/java/it/logostech/wristbandproject/app/model/payment/exceptions/PaymentProtocolMessageException.java
@@ -0,0 +1,10 @@
+package it.logostech.wristbandproject.app.model.payment.exceptions;
+
+/**
+ * Created by michele.schimd on 04/09/2014.
+ */
+public class PaymentProtocolMessageException extends Exception {
+    public PaymentProtocolMessageException(String msg) {
+        super(msg);
+    }
+}
