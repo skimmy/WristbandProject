afe8a63449c210c8d8834b12a123328ef60a9925
Michele Schimd <michele.schimd@gmail.com>
2014-09-22 16:52:50 +0200

    Further advances on NFC communication

--- app/src/main/java/it/logostech/wristbandproject/app/deamons/PaymentSystemDaemon.java
+++ app/src/main/java/it/logostech/wristbandproject/app/deamons/PaymentSystemDaemon.java
@@ -1,5 +1,7 @@
 package it.logostech.wristbandproject.app.deamons;
 
+import android.nfc.tech.IsoDep;
+
 import it.logostech.wristbandproject.app.model.TagModel;
 
 /**
@@ -11,20 +13,70 @@ public class PaymentSystemDaemon {
 
     private static TagModel currentTag = null;
 
-    public static void tagDiscovered(TagModel tag) {
+    /**
+     * This method is called when a tag is discovered (it can be either a new
+     * one, or one already known). Since the communication on NFC channels are
+     * usually <i>half duplex</i>, this method automatically ignores a tag when
+     * there is one already in process. The method also indicated whether the
+     * passed tag has been ignored (return value <code>false</code>) or it has
+     * been set as the new current tag (return value <code>false</code>).
+     *
+     * @param tag the discovered tag
+     * @return <code>true</code> if the passed tag is the new current tag and
+     * <code>false</code> otherwise
+     *
+     */
+    public static boolean tagDiscovered(TagModel tag) {
         // Check if the Id corresponds with the id that is actually in an active
         // NFC communication (if any).
         if (currentTag != null && currentTag.equals(tag)) {
             //
         } else {
-            // new tag or mismatch
+            // new tag...
             if (currentTag == null) {
-                // there is no current tag so we insert the new one
                 currentTag = tag;
+                simpleNfcCommunication(currentTag);
+               // ...mismatch
             } else {
                 // there already exist a tag so we discard the current one
-                return;
+                return false;
             }
+        }
+        return true;
+    }
+
+    /**
+     * Resets the current tag if and only if the actual current tag has the same
+     * id as the passed one.
+     *
+     * @param tag the tag to be reset
+     * @return <code>true</code> if and only if the current tag has been reset
+     */
+    public static boolean discardTag(TagModel tag) {
+        if (currentTag.equals(tag)) {
+            currentTag = null;
+            return  true;
+        }
+        return false;
+    }
+
+    /**
+     * Performs a NFC communication payment protocol with the tag described by
+     * the passed parameter.
+     *
+     * @param tagModel the tag identifying the NFC counterpart
+     */
+    private static void simpleNfcCommunication(TagModel tagModel) {
+        // try to set up a communication channel
+        if (tagModel.getNfcTag() == null) {
+            throw new IllegalArgumentException("Nfc communication needs low level Tag object, found null");
+        }
+        IsoDep isoDep = IsoDep.get(tagModel.getNfcTag());
+
+        if (isoDep != null) {
+
+        } else {
+            // TODO: Do something when the channel can not be established
         }
     }
 }
--- app/src/main/java/it/logostech/wristbandproject/app/util/TypeUtil.java
+++ app/src/main/java/it/logostech/wristbandproject/app/util/TypeUtil.java
@@ -1,7 +1,5 @@
 package it.logostech.wristbandproject.app.util;
 
-import java.math.BigInteger;
-
 /**
  * Created by michele.schimd on 22/09/2014.
  */
@@ -11,10 +9,19 @@ public class TypeUtil {
      * useful for debugging purpose since it allows to have a visual
      * representation of a byte array.
      *
-     * @param array inpu array
+     * @param bytes input array
      * @return the hexadecimal encoding of the input array
      */
-    public static String byteArrayToHexString(byte[] array) {
-        return new BigInteger(array).toString(16).toUpperCase();
+    public static String byteArrayToHexString(byte[] bytes) {
+        final char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] hexChars = new char[bytes.length * 2];
+        int v;
+        for ( int j = 0; j < bytes.length; j++ ) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+//        return new BigInteger(array).toString();
     }
 }
