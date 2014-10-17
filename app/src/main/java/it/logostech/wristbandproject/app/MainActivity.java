a32944a13463ec13e6e467df105bebbd945d8d85
Michele Schimd <michele.schimd@gmail.com>
2014-10-17 11:06:37 +0200

    MainActivity: cleaned from "old" save/load tag(s) stuff

--- app/src/main/java/it/logostech/wristbandproject/app/MainActivity.java
+++ app/src/main/java/it/logostech/wristbandproject/app/MainActivity.java
@@ -26,22 +26,17 @@ import java.math.BigInteger;
 import java.util.LinkedList;
 import java.util.List;
 
+import it.logostech.wristbandproject.app.debug.WebServiceDebugActivity;
 import it.logostech.wristbandproject.app.model.TagModel;
 import it.logostech.wristbandproject.app.nfc.NfcUtil;
 import it.logostech.wristbandproject.app.util.DialogResponder;
 import it.logostech.wristbandproject.app.util.TagUtility;
 
 
-public class MainActivity extends ActionBarActivity implements DialogResponder {
+public class MainActivity extends ActionBarActivity {
 
-    private TagModel selectedTag = null;
-    private List<TagModel> tagsList;
-    private TagModel readTag = null;
-
-    private static final String savedTagFileName = "_saved_tags.dat";
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private ArrayAdapter<TagModel> mAdapter = null;
 
 
     @Override
@@ -49,19 +44,12 @@ public class MainActivity extends ActionBarActivity implements DialogResponder {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         // check (eventually forcing) default service for AID
         String aid = getResources().getString(R.string.nfcAID);
         if (!NfcUtil.isDefaultServiceForAid(aid, this)) {
             NfcUtil.setDefaultForAid(aid, this);
         }
 
-
-
-        // --------------------------------------------------------------------------------------
-        // After migration to different activities this code (and few more things) will remain
-        // --------------------------------------------------------------------------------------
 
         // Start the wireless activity
         Button wirelessActivityButton = (Button) findViewById(R.id.wirelessActivityButton);
@@ -92,119 +80,28 @@ public class MainActivity extends ActionBarActivity implements DialogResponder {
             }
         });
 
-        // --------------------------------------------------------------------------------------
-
-
-        Button saveButton = (Button) findViewById(R.id.saveTagIdButton);
-        saveButton.setOnClickListener(new View.OnClickListener() {
+        Button wsDebugActivityButton = (Button) findViewById(R.id.wsDebugActivityButton);
+        wsDebugActivityButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("MainActivity", "Clicked!");
-                if (readTag != null) {
-                    selectedTag = readTag;
-                }
-                if (selectedTag != null) {
-                    (new SaveTagDialogFragment(selectedTag)).show(getFragmentManager(), "AA");
-                }
+                Intent intent = new Intent(MainActivity.this, WebServiceDebugActivity.class);
+                startActivity(intent);
             }
         });
 
 
-        this.tagsList = new LinkedList<TagModel>();
-
-        // initialization of the list view
-        ListView tagsListView = (ListView) findViewById(R.id.savedTagListView);
-        tagsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Object clicked = adapterView.getItemAtPosition(i);
-                Log.v("MainActivity", "onItemClick " + clicked.toString());
-                selectedTag = (TagModel) clicked;
-            }
-        });
-
-        final TextView simulationTextView = (TextView) findViewById(R.id.simulationStateText);
-
-        Button startSimulationButton = (Button) findViewById(R.id.startSimulationButton);
-        startSimulationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String id = "None";
-                if (selectedTag != null) {
-                    id = selectedTag.getStringId();
-                }
-                Log.v("Simulation Start Button", "cliked");
-                simulationTextView.setText("Simulating on " + id);
-            }
-        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        String p = Environment.getDataDirectory().getAbsolutePath();
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(getIntent().getAction())) {
-            Tag t = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            Log.v("MainActivity", "Tag Discovered");
-            for (String tech : t.getTechList()) {
-                Log.v("MainActivity", " -- " + tech);
-            }
-            TagModel tag = new TagModel(t.getId());
-            TextView tw = (TextView) findViewById(R.id.idTextView);
-            tw.setText(new BigInteger(tag.getId()).toString());
-
-            TagUtility.saveTagToFile(tag, "test", this);
-
-            TagModel loadedTag = TagUtility.loadTagFromFile("test", this);
-            String tmp = (new BigInteger(loadedTag.getId()).toString());
-            TextView loadedTW = (TextView) findViewById(R.id.loadTagTextView);
-            loadedTW.setText(tmp);
-
-            this.readTag = tag;
-        }
-        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
-            Tag t = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            Log.v("MainActivity", "Tech Discovered");
-            for (String tech : t.getTechList()) {
-                Log.v("MainActivity", " -- " + tech);
-            }
-            for (String tech : t.getTechList()) {
-                Log.v("MainActivity", " -- " + tech);
-            }
-        }
-
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            Tag t = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            Log.v("MainActivity", "NDEF Discovered");
-            for (String tech : t.getTechList()) {
-                Log.v("MainActivity", " -- " + tech);
-            }
-        }
-
-        try {
-            this.tagsList = this.loadSavedTags();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Log.v("onResume", "Loaded " + this.tagsList.size() + " tags!");
-
-        TagModel[] tagsArray = new TagModel[this.tagsList.size()];
-        tagsArray = this.tagsList.toArray(tagsArray);
-        ListView tagsListView = (ListView) findViewById(R.id.savedTagListView);
-        this.mAdapter = new ArrayAdapter<TagModel>(
-                this, android.R.layout.simple_list_item_1, tagsArray);
-        tagsListView.setAdapter(mAdapter);
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        try {
-            this.saveTags(this.tagsList);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 
     @Override
@@ -221,58 +118,5 @@ public class MainActivity extends ActionBarActivity implements DialogResponder {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
-    }
-
-    private List<TagModel> loadSavedTags() throws IOException {
-        FileInputStream fis = this.openFileInput(savedTagFileName);
-        DataInputStream dis = new DataInputStream(fis);
-        int tagCount = dis.readInt();
-        List<TagModel> tags = new LinkedList<TagModel>();
-
-        for (int i = 0; i < tagCount; ++i) {
-
-            int length = dis.readInt();
-            byte[] id = new byte[length];
-            dis.read(id);
-
-            int nameArrayLength = dis.readInt();
-            byte[] name = new byte[nameArrayLength];
-            dis.read(name);
-            String tagName = new String(name);
-
-            tags.add(new TagModel(id, tagName));
-
-        }
-        dis.close();
-        fis.close();
-
-        return tags;
-    }
-
-    private void saveTags(List<TagModel> tags) throws IOException {
-        FileOutputStream fos = this.openFileOutput(savedTagFileName, MODE_PRIVATE);
-        DataOutputStream dos = new DataOutputStream(fos);
-        int tagCount = tags.size();
-        dos.writeInt(tagCount);
-        for (TagModel tag : tags) {
-            dos.writeInt(tag.getId().length);
-            dos.write(tag.getId());
-            dos.writeInt(tag.getName().getBytes().length);
-            dos.write(tag.getName().getBytes());
-        }
-        dos.close();
-        fos.close();
-    }
-
-    @Override
-    public void onDialogPositiveClick(DialogFragment dialog) {
-        Log.v("SaveTagDialog", "Sure!!");
-        TagModel tagToSave = ((SaveTagDialogFragment) dialog).getTagToSave();
-        this.tagsList.add(tagToSave);
-    }
-
-    @Override
-    public void onDialogNegativeClick(DialogFragment dialog) {
-        Log.v("SaveTagDialog", "Naaah!!");
     }
 }
