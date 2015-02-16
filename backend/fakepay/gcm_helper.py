33b66d5be7849bd2127e08b928daa70d3bbc8ca8
Michele Schimd <michele.schimd@gmail.com>
2015-02-16 14:30:18 +0100

    First working GCM location message

--- backend/fakepay/gcm_helper.py
+++ backend/fakepay/gcm_helper.py
@@ -1,14 +1,48 @@
 """This package contais helper functions to interact with Cloud Connection Server
 (CCS) used for interacting with devices via Goggle Cloud Messaging (GCM)"""
 
+import urllib2
+import json
+
 # address of CCS
 CCS_SERVER = "gcm-preprod.googleapis.com"
 CCS_PORT = 5236
+# URL of HTTP GCM server
+HTTP_SERVER_URL = "https://android.googleapis.com/gcm/send"
+
 USERNAME = "WristbandProject.Backend"
-PASSWORD = "AIzaSyC22aO3e5BzTTzXZZycXuORDtOBJpH0_m4"
+API_KEY = "AIzaSyC22aO3e5BzTTzXZZycXuORDtOBJpH0_m4"
 REGISTRATION_ID = "WristbandProject.Tutor"
 # "gcm.googleapis.com:5235" <-- Production address
 
+def getHttpRequestHeader():
+    hds = {
+        'Authorization': 'key=' + API_KEY,
+        'Content-Type': 'application/json'
+        }
+    return hds
+
+def getHttpRequestData(locModel, regIds):
+    data = {
+#    "registration_ids" : ("[ " + (", ".join(regIds)) + " ]")
+        "registration_ids" :  regIds,
+        "latitude" : str(locModel.latitude),
+        "longitude" : str(locModel.longitude)
+     }
+    return json.dumps(data)
+
+def sendGcmMessage(header, data):
+    req = urllib2.Request(HTTP_SERVER_URL,  data, header)
+    print(repr(req))
+    try:
+        reply = urllib2.urlopen(req)
+        print(repr(reply))
+    except urllib2.URLError, e:
+        print(repr(e.reason))
 
 def gcm_send_location(locModel):
-    pass
+    header = getHttpRequestHeader()
+    data = getHttpRequestData(locModel, [REGISTRATION_ID])
+#    print ("[HEADER] " + str(header))
+#    print ("[DATA]   " + str(data))
+    sendGcmMessage(header,data)
